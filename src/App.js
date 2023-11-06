8d4fb9af0910941402ed3c317f506d118515ec56
Raihan <abdurraihan565@gmail.com>
2023-11-06 11:08:58 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Layout from './Layout';
@@ -11,18 +11,20 @@ import Fitness from './pages/Fitness';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" elements={Layout}>
-          <Route path="/" element={<Home />} />
-          <Route path="/Fitness-Datails/:slug" element={<Fitness />} />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" elements={Layout}>
+            <Route path="/" element={<Home />} />
+            <Route path="/Fitness-Datails/:slug" element={<Fitness />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
